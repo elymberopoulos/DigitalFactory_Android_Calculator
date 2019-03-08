87ccf19559c9a03eaab0ca751ae58836a30bfc41
Eric <elymberopoulos@luc.edu>
2019-03-08 17:52:01 -0600

    Initial math operations tied into the UI. Styling has been refactored. unnecessary UI elements removed.

--- www/js/index.js
+++ www/js/index.js
@@ -1,10 +1,10 @@
-// require('../css/index.css');
-const mathBasic = require('../../mathFramework/basicMath/BasicMath');
+require('../css/index.css');
+const mathModule = require('../../mathFramework/basicMath/BasicMath');
 const $ = require('jquery');
 
 $(document).ready(function () {
 
-    var calculateAnswer = $('#calculateAnswer').on("click", myfunction);
+    var calculateAnswer = $("#calculateBTN").on("click", myfunction);
     var operatorBtn = $(".mathButton").on("click", setMathOperation);
     var mathOperation = "";
 
@@ -12,17 +12,37 @@ $(document).ready(function () {
 
     function setMathOperation() {
         mathOperation = this.value;
-        alert(mathOperation);
     }
 
     function myfunction() {
-        alert('working fine');
-        switch (mathSelector) {
+
+        var valueOne = parseFloat($("#valueOne").val());
+        var valueTwo = parseFloat($("#valueTwo").val());
+
+        switch (mathOperation) {
             case "addition":
-
+                var result = mathModule.add(valueOne, valueTwo);
+                alert(result);
                 break;
-
+            case "subtraction":
+                alert('subtraction');
+                break;
+            case "multiplication":
+                alert('multiplication');
+                break;
+            case "division":
+                alert('division');
+                break;
+            case "sine":
+                alert('sine');
+                break;
+            case "cosine":
+                alert('cosine');
+                break;
+            case "tangent":
+                alert('tangent');
             default:
+                alert("Please select an operation");
                 break;
         }
     }
