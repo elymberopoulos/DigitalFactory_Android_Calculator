a076096efdc2e931671a16b12a9d9411bef50614
Eric <elymberopoulos@luc.edu>
2019-03-08 14:37:35 -0600

    Initial styling added and event listeners added for math operator selection

--- www/js/index.js
+++ www/js/index.js
@@ -5,15 +5,23 @@ const $ = require('jquery');
 $(document).ready(function () {
 
     var calculateAnswer = $('#calculateAnswer').on("click", myfunction);
-    var mathSelector = "";
+    var operatorBtn = $(".mathButton").on("click", setMathOperation);
+    var mathOperation = "";
+
+
+
+    function setMathOperation() {
+        mathOperation = this.value;
+        alert(mathOperation);
+    }
 
     function myfunction() {
         alert('working fine');
         switch (mathSelector) {
             case "addition":
-                
+
                 break;
-        
+
             default:
                 break;
         }
